e7adaa42ced0da40137095597fb45768c2635600
OfficialBrot <36410565+OfficialFreak@users.noreply.github.com>
2026-01-15 22:19:49 +0100

    Add argparse support and renew option to downloader
    Introduced argparse to handle command-line arguments and added a --renew flag to control re-downloading of profile pictures. The script now skips already downloaded images unless the renew option is specified.

--- profile_pic_downloader.py
+++ profile_pic_downloader.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import mimetypes
 import os
@@ -8,11 +9,34 @@ from tqdm import tqdm
 EXPORT_FOLDER = "export"
 IMAGE_FOLDER = "images"
 
+parser = argparse.ArgumentParser(
+    prog="WhatsApp Profile Pic Downloader",
+    description="Downloads profile pictures from your given contacts.json",
+)
+parser.add_argument("-r", "--renew", action="store_true")
+args = parser.parse_args()
+
+
+def remove_file_ending(filename):
+    return ".".join(filename.split(".")[:-1])
+
+
+downloaded_ids = set(
+    [
+        remove_file_ending(f)
+        for f in os.listdir(IMAGE_FOLDER)
+        if os.path.isfile(os.path.join(IMAGE_FOLDER, f))
+    ]
+)
+
 if not os.path.exists(IMAGE_FOLDER):
     os.makedirs(IMAGE_FOLDER)
 
 with open(os.path.join(EXPORT_FOLDER, "contacts.json"), encoding="utf-8") as f:
     contacts = json.load(f)
+
+    if not args.renew:
+        contacts = {k: v for k, v in contacts.items() if k not in downloaded_ids}
 
     for contact, metadata in tqdm(contacts.items()):
         if x := metadata.get("profilePicUrl"):
